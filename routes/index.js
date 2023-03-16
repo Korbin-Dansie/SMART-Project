bc08ea6ac2b98295e278be9132e8cff1eca4bd3f
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-03-16 15:51:39 -0600

    Display some database text
    Get some text from the database and display it

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var dbCon = require('../lib/database');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let databaseString;
+
+  let sql = "SELECT some_text FROM test";
+  dbCon.query(sql, function (err, rows) {
+  if (err) {
+    console.log(err.message);
+      throw err;
+  }
+
+  databaseString = rows[0].some_text;
+
+  res.render('index', { title: 'Express', databaseString: databaseString });
+
+  });
 });
 
 module.exports = router;
