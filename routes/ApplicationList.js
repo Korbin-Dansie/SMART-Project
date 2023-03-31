c9e177dfcef6a07ce3f71216717c2acf8d2bd447
Korbin Dansie <dansiekm@gmail.com>
2023-03-30 20:58:25 -0600

    Created application list

--- routes/ApplicationList.js
+++ routes/ApplicationList.js
@@ -6,7 +6,13 @@ var dbCon = require("../lib/database");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   console.log("Get ApplicationList.js");
-  res.render("ApplicationList", {});
+  var person1 = {name: "Jane Doe", age: 14, grade: 8, date_of_application: (new Date(2023, 3, 27)).toLocaleDateString('en-US'), application_status:"Active", application_id:1};
+  var person2 = {name: "Peter Tamiko", age: 15, grade: 7, date_of_application: (new Date(2023, 2, 16)).toLocaleDateString('en-US'), application_status:"Wait listed", application_id:2};
+  var person3 = {name: "Jase Mathew", age: 14, grade: 8, date_of_application: (new Date(2023, 2, 11)).toLocaleDateString('en-US'), application_status:"Rejected", application_id:3};
+
+  let applications = [person1, person2, person3];
+
+  res.render("ApplicationList", {applications: applications});
 });
 
 module.exports = router;
