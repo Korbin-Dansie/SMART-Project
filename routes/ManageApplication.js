aa6b6a8183d8726d467ce8bcc33a3ace48074790
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-04-06 21:09:27 -0600

    Sending Contact Info and Guardian Info to ManageApplication
    Now sending contact information and guardian information from the database to the Manage Application page. Populating database with dummy data for now.

--- routes/ManageApplication.js
+++ routes/ManageApplication.js
@@ -10,15 +10,17 @@ router.get("/", function (req, res, next) {
   let applicationID = req.query.applicationID;
 
   if (applicationID) {
-    const sql = "CALL select_application_details(" + applicationID + ")";
-    dbCon.query(sql, function (err, results) {
+    let sql = "CALL select_application_details(?); CALL get_personid_from_applicationid(?, @personid); CALL select_contact_info(@personid); CALL select_guardians(?);";
+    dbCon.query(sql, [applicationID, applicationID, applicationID], function (err, results) {
       if (err) {
         throw err;
       }
       //console.log(results);
       console.log(results[0][0]);
+      console.log(results[3][0]);
+      console.log(results[5][0]);
   
-      res.render('ManageApplication', {applicationData: results[0][0]});
+      res.render('ManageApplication', {applicationData: results[0][0], contactInfo: results[3][0]});
     });
   } else {
     res.redirect('/');
