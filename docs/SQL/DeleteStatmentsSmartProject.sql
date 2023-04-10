3627226ef693476f3fd9de53d449ad7f1faec83e
Korbin Dansie <dansiekm@gmail.com>
2023-04-10 12:22:06 -0600

    Working on creating classes

--- docs/SQL/DeleteStatmentsSmartProject.sql
+++ docs/SQL/DeleteStatmentsSmartProject.sql
@@ -4,16 +4,15 @@ SELECT CONCAT('DROP TABLE IF EXISTS `', table_name, '`;')
 FROM information_schema.tables
 WHERE table_schema = 'smart_project';
 
-SELECT COUNT(CONCAT('DROP TABLE IF EXISTS `', table_name, '`;'))
-FROM information_schema.tables
-WHERE table_schema = 'smart_project';
+SELECT CONCAT('DROP TRIGGER IF EXISTS `', trigger_name, '`;')
+from information_schema.triggers
+WHERE trigger_schema = 'smart_project';
 
 SELECT CONCAT('DROP PROCEDURE IF EXISTS `', routine_name, '`;')
 from information_schema.routines 
 WHERE routine_type = 'PROCEDURE' 
 AND routine_schema = 'smart_project';
 
-SELECT COUNT(CONCAT('DROP TRIGGER IF EXISTS `', routine_name, '`;'))
-from information_schema.routines 
-WHERE routine_type = 'TRIGGER' 
-AND routine_schema = 'smart_project';
+
+
+
--- routes/NewCourse.js
+++ routes/NewCourse.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
+var dbCon = require("../lib/database");
+
+
 /* GET new course page. */
 router.get('/', function (req, res, next) {
-    res.render('NewCourse', {});
+    let obj = new Object();
+
+    getNewCoursePart1(req, res, obj);
 });
 
+// Load the semesters
+function getNewCoursePart1(req, res, obj){
+    const sql = "CALL `get_semesters`();";
+    dbCon.query(sql, function (err, results) {
+        if (err) {
+          throw err;
+        }
+        console.log(results[0]);
+
+        let semesters = new Array();
+        results[0].forEach(element => {
+            semesters.push(
+                {...element}
+            );
+        });
+        obj.semesters = semesters;
+
+        getNewCoursePart2(req, res, obj);
+    });
+}
+
+// Load the classes
+function getNewCoursePart2(req, res, obj){
+    const sql = "CALL `get_classes`();";
+    dbCon.query(sql, function (err, results) {
+        if (err) {
+          throw err;
+        }
+        console.log(results[0]);
+
+        let classes = new Array();
+        results[0].forEach(element => {
+            classes.push(
+                {...element}
+            );
+        });
+        obj.classes = classes;
+
+        // The spread operator ... puts all the values in like "semesters: obj.semesters" so I dont have to do it manualy
+        res.render('NewCourse', {...obj});
+    });
+}
 module.exports = router;
