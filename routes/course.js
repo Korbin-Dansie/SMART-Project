ddcc433a93d50a4531925b4e4f5e96dd05ec15d1
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-04-12 14:51:10 -0600

    Course Page Loads Course Data

--- routes/course.js
+++ routes/course.js
@@ -14,8 +14,10 @@ router.get('/', function (req, res, next) {
       }
 
       console.log(results);
+      console.log(results[0]);
+      console.log(results[1]);
 
-      res.render('Course', {});
+      res.render('Course', {classTimes: results[0], classData: results[1][0]});
     });
 });
 
