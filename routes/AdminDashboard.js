b6059097ce1faaee7a5e461ec5382f0f59bfa48b
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-04-14 12:29:43 -0600

    Removed Application List from Admin Dashboard
    Added a button to the Admin Dashboard to navigate to the ApplicationList page rather than display the list on both pages. This flow makes more sense in my opinion

--- routes/AdminDashboard.js
+++ routes/AdminDashboard.js
@@ -4,12 +4,13 @@ var router = express.Router();
 /* GET application form page. */
 router.get('/', function (req, res, next) {
 
-    var person1 = {name: "Jane Doe", age: 14, grade: 8, date_of_application: (new Date(2023, 3, 27)).toLocaleDateString('en-US')};
-    var person2 = {name: "Peter Tamiko", age: 15, grade: 7, date_of_application: (new Date(2023, 2, 16)).toLocaleDateString('en-US')};
-    var person3 = {name: "Jase Mathew", age: 14, grade: 8, date_of_application: (new Date(2023, 2, 11)).toLocaleDateString('en-US')};
+    // var person1 = {name: "Jane Doe", age: 14, grade: 8, date_of_application: (new Date(2023, 3, 27)).toLocaleDateString('en-US')};
+    // var person2 = {name: "Peter Tamiko", age: 15, grade: 7, date_of_application: (new Date(2023, 2, 16)).toLocaleDateString('en-US')};
+    // var person3 = {name: "Jase Mathew", age: 14, grade: 8, date_of_application: (new Date(2023, 2, 11)).toLocaleDateString('en-US')};
 
-    let applications = [person1, person2, person3];
-    res.render('AdminDashboard', {applications: applications});
+    // let applications = [person1, person2, person3];
+    
+    res.render('AdminDashboard', {});
 });
 
 module.exports = router;
