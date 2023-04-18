8247f5900ce396b29156b98e62db89d1de548a8f
BrieFishy <112446655+BrieFishyW@users.noreply.github.com>
2023-04-17 20:37:07 -0600

    Info pulls from database, doesn't show on page yet

--- routes/PersonList.js
+++ routes/PersonList.js
@@ -20,27 +20,68 @@ router.get('/addStudent', function(req, res, next) {
       throw err;
     }
 
-    console.log(results);
     console.log(results[0]);
-    console.log(results[1]);
+    console.log('========================================================');
 
     // Get personList to dynamically change to add students to certain class times based on what class times are in the class
     let sql2 = "CALL select_class_times(?);";
-    dbCon.query(sql, [classID], function (err, results2) {
+    dbCon.query(sql2, [classID], function (err, results2) {
       if (err) {
         throw err;
       }
   
-      console.log(results2);
       console.log(results2[0]);
-      console.log(results2[1]);
-    //res.render('Course', {classTimes: results[0], classData: results[1][0], classID: classID});
+      parseStudentClassInfo(results[0], results2[0]);
+
     });
   });
-  // Display the page with add/remove buttons
-  res.render('PersonList', );
 
-  // TODO: set it up to recieve a POST to add/remove students then redirect to the right course page again
+  // TODO: set it up to recieve a POST to add/remove students then show the page again
 });
+
+function parseStudentClassInfo(studentTable, class_time_table){
+  // Iterate through the class times, make an array of them and find the highest group number
+  var groups = 0;
+  var class_time_list = [];
+  class_time_table.forEach(function (classTime) {
+    if (classTime["group"] > groups) groups = classTime["group"];
+    var weekday = classTime['name'].slice(0,3);
+    var startTime = classTime["start_time"].slice(0,5);
+    var endTime = classTime["end_time"].slice(0,5);
+    class_time_list.push({id:classTime["class_time_id"], value: (weekday + " " + startTime + "-" + endTime)})
+  });
+  // Create a better table of all students
+  var studentArray = [];
+  studentTable.forEach(function (student) {
+    // Make sure the student isn't already in the list we have
+    let processedStudent = false;
+    var studentObject;
+    for (i = 0; i < studentArray.length; i ++){
+      if (studentArray[i]['ID'] == student['student_id']){
+        processedStudent = true;
+        studentObject = studentArray[i]['ID'];
+        break;
+      }
+    }
+    if (!processedStudent){
+      // Add the student to the set
+      studentObject = {ID: student["student_id"], name: student["first_name"] + " " + student["last_name"]}
+      for (i = 0; i < groups; i ++){
+        studentObject['group_'+i] = null;
+      }
+      studentArray.push(studentObject);
+    } 
+    var class_time = student["class_time_id"];
+    if (class_time != null) {
+      // Find what group the class time is in
+      var class_group = student["group"];
+      // Add the new class time to the student
+      studentObject['group_'+class_group] = class_time;
+    }
+  });
+
+  // Display the page with add/remove buttons
+  res.render('AddStudent', );
+}
 
 module.exports = router;
