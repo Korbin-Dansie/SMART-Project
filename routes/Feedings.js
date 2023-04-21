85a19e479c70fa540356659344b7aa877201fc2b
Korbin Dansie <dansiekm@gmail.com>
2023-04-21 17:15:32 -0600

    sending the post, but not sending form data

--- routes/Feedings.js
+++ routes/Feedings.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var dbCon = require('../lib/database');
+var dbCon = require("../lib/database");
 
 /* GET list of students that need feeding */
-router.get('/students', function(req, res, next) {
+router.get("/students", function (req, res, next) {
   let sql = "CALL get_students_with_meal_assistance();";
   dbCon.query(sql, function (err, results) {
     if (err) {
@@ -12,19 +12,17 @@ router.get('/students', function(req, res, next) {
     }
 
     let students = new Array();
-    results[0].forEach(element => {
-      students.push(
-            {...element}
-        );
+    results[0].forEach((element) => {
+      students.push({ ...element });
     });
     return res.send(Object.values(students));
   });
 });
 
 /* GET list of students that are feed today */
-router.get('/date', function(req, res, next) {
+router.get("/date", function (req, res, next) {
   let date = req.query.date;
-  if(date == undefined){
+  if (date == undefined) {
     date = null;
   }
   let sql = "CALL get_feedings(?);";
@@ -34,28 +32,34 @@ router.get('/date', function(req, res, next) {
     }
 
     // If we did not send a date return null
-    if(results[0] == undefined){
+    if (results[0] == undefined) {
       return res.send([]);
     }
 
     let feedings = new Array();
-    results[0].forEach(element => {
-      feedings.push(
-            {...element}
-        );
+    results[0].forEach((element) => {
+      feedings.push({ ...element });
     });
     return res.send(Object.values(feedings));
   });
 });
 
+/* GET list of students that are feed today */
+router.post("/insertFeedings", function (req, res, next) {
+  console.log(req.body);
+  console.log("Here");
+
+  return res.send([]);
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   let obj = new Object();
   getStep1(req, res, obj);
 });
 
 // Load the meal times
-function getStep1(req, res, obj){
+function getStep1(req, res, obj) {
   let sql = "CALL `get_meal_times`()";
   dbCon.query(sql, function (err, results) {
     if (err) {
@@ -63,19 +67,17 @@ function getStep1(req, res, obj){
     }
 
     let meal_times = new Array();
-    results[0].forEach(element => {
-      meal_times.push(
-            {...element}
-        );
+    results[0].forEach((element) => {
+      meal_times.push({ ...element });
     });
 
     obj.meal_times = meal_times;
-    getStep2(req, res, obj)
+    getStep2(req, res, obj);
   });
 }
 
 // Load the current students in need of meal assistance
-function getStep2(req, res, obj){
+function getStep2(req, res, obj) {
   let sql = "CALL get_students_with_meal_assistance();";
   dbCon.query(sql, function (err, results) {
     if (err) {
@@ -83,20 +85,18 @@ function getStep2(req, res, obj){
     }
 
     let students = new Array();
-    results[0].forEach(element => {
-      students.push(
-            {...element}
-        );
+    results[0].forEach((element) => {
+      students.push({ ...element });
     });
 
     obj.students = students;
-    getStep3(req, res, obj)
+    getStep3(req, res, obj);
   });
 }
 
 // Load if anybody has already been feed
-function getStep3(req, res, obj){
-    res.render('Feedings', {...obj});
+function getStep3(req, res, obj) {
+  res.render("Feedings", { ...obj });
 }
 
 module.exports = router;
