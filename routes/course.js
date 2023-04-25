a9b6e90f0c370e10b4561cf79e507e5d17b3df22
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-04-24 20:18:13 -0600

    Course Page Load Course Assignments

--- routes/course.js
+++ routes/course.js
@@ -7,8 +7,8 @@ var dbCon = require('../lib/database');
 router.get('/', function (req, res, next) {
     let classID = req.query.classID;
 
-    let sql = "CALL select_class(?); CALL select_class_distinct_students(?);";
-    dbCon.query(sql, [classID, classID], function (err, results) {
+    let sql = "CALL select_class(?); CALL select_class_distinct_students(?); CALL select_class_assignments(?);";
+    dbCon.query(sql, [classID, classID, classID], function (err, results) {
       if (err) {
         throw err;
       }
@@ -17,7 +17,7 @@ router.get('/', function (req, res, next) {
       //console.log(results[0]);
       //console.log(results[1]);
 
-      res.render('Course', {classTimes: results[0], classData: results[1][0], classStudents: results[3]});
+      res.render('Course', {classTimes: results[0], classData: results[1][0], classStudents: results[3], classAssignments: results[5]});
     });
 });
 
