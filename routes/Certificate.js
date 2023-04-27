7c8a495591d3bfad0ac6bdd8ea803144689a1942
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-04-26 19:42:48 -0600

    View Student Certificate from Gradebook

--- routes/Certificate.js
+++ routes/Certificate.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var dbCon = require('../lib/database');
+
 /* GET certificate page. */
 router.get('/', function (req, res, next) {
+    let classID = req.query.classID;
+    let studentID = req.query.studentID;
+
+    let sql = "CALL select_student_certificate(?, ?);";
+
+    dbCon.query(sql, [classID, studentID], function (err, results) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(results);
+
+        res.render('Certificate', {classDetails: results[1][0], certificateDetails: results[2][0]});
+    });
+});
+
+router.post('/', function (req, res, next) {
     res.render('Certificate', {});
 });
 
