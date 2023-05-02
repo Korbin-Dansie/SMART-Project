6a9b697d9c01f55cc1631e03e5c2e42114cb1847
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-05-01 20:56:20 -0600

    Change User Password

--- routes/ManageAccount.js
+++ routes/ManageAccount.js
@@ -23,24 +23,42 @@ router.get("/", function (req, res, next) {
 router.post("/", function (req, res, next) {
   let userID = req.query.userID;
 
-  let firstName = req.body.userFirstName;
-  let lastName = req.body.userLastName;
-  let email = req.body.userEmail;
-  let accountType = req.body.accountType;
-  let isActive = req.body.isAccountActive;
-
-  let sql = "CALL update_user_details(?, ?, ?, ?, ?, ?);"
-  dbCon.query(sql, [userID, firstName, lastName, email, accountType, isActive], function (err, results) {
-    if (err) {
-      throw err;
-    }
-    
-    console.log(results);
-
-    let dataToPass = encodeURIComponent(userID);
-    res.redirect('/ManageAccount?userID=' + dataToPass);
-  });
+  if (req.body.firstName === undefined) {
+    let salt = req.body.salt;
+    let hash = req.body.hash;
   
+    let sql = "CALL change_user_password(?, ?, ?);"
+    dbCon.query(sql, [userID, salt, hash], function (err, results) {
+      if (err) {
+        throw err;
+      }
+      
+      console.log(results);
+  
+      let dataToPass = encodeURIComponent(userID);
+      res.redirect('/ManageAccount?userID=' + dataToPass);
+    });
+  } else {
+    let firstName = req.body.userFirstName;
+    let lastName = req.body.userLastName;
+    let email = req.body.userEmail;
+    let accountType = req.body.accountType;
+    let isActive = req.body.isAccountActive;
+  
+    let sql = "CALL update_user_details(?, ?, ?, ?, ?, ?);"
+    dbCon.query(sql, [userID, firstName, lastName, email, accountType, isActive], function (err, results) {
+      if (err) {
+        throw err;
+      }
+      
+      console.log(results);
+  
+      let dataToPass = encodeURIComponent(userID);
+      res.redirect('/ManageAccount?userID=' + dataToPass);
+    });
+  }
+
 });
+
 
 module.exports = router;
