325a1f1c9ec779680c427825f51eaf32f5e6edee
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-05-03 21:26:24 -0600

    Owner General Statistics Report

--- routes/OwnerDashboard.js
+++ routes/OwnerDashboard.js
@@ -6,7 +6,7 @@ var dbCon = require('../lib/database');
 /* GET application form page. */
 router.get('/', function (req, res, next) {
 
-    let sql = "CALL select_subjects(); CALL select_subject_class_counts(); CALL select_subject_certificate_counts();";
+    let sql = "CALL select_subjects(); CALL select_subject_class_counts(); CALL select_subject_certificate_counts(); CALL get_program_statistics();";
     dbCon.query(sql, function (err, results) {
       if (err) {
         throw err;
@@ -16,7 +16,15 @@ router.get('/', function (req, res, next) {
       //console.log(results[0]);
       //console.log(results[1]);
 
-      res.render('OwnerDashboard', {certificateSubjects: results[0], classSubjectCounts: results[2], certificateSubjectCounts: results[4]});
+      let generalStatistics = []
+
+      for (let i = 6; i < results.length - 1; i++) {
+        generalStatistics.push(results[i][0]);
+      }
+
+      console.log(generalStatistics);
+
+      res.render('OwnerDashboard', {certificateSubjects: results[0], classSubjectCounts: results[2], certificateSubjectCounts: results[4], generalStatistics: generalStatistics});
     });
 });
 
